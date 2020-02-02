1b5008750df7aeebdf1bfc49905413b047ab1814
scorpionmanace <karan.spartan90@gmail.com>
2020-02-02 05:19:00 -0800

    Error message handling

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -5,13 +5,16 @@ import QuizQuestionButton from './QuizQuestionButton';
 class QuizQuestion extends Component {
 
     constructor(props) {
-        super(props)
-        console.log(this.props);
+        super(props);
+        this.state = {incorrectAnswer: false};
     }
 
     handleClick(buttonText) {
         if (buttonText === this.props.quiz_question.answer) {
+            this.setState(() => {return {incorrectAnswer: false}});
             this.props.showNextQuestionHandler();
+        } else {
+            this.setState(() => {return {incorrectAnswer: true}});
         }
     }
 
@@ -28,6 +31,7 @@ class QuizQuestion extends Component {
                   }
               </ul>
             </section>
+            {this.state.incorrectAnswer?<div className="error">Sorry, that's not right</div>:null}
           </main>
         );
     }
